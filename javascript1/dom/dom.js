786db602cad54047a81f7cbe21feb86457ddfed6
FabiolaSoliz <cb.faviola.soliz.t@upds.net.bo>
2022-03-02 10:26:00 -0400

    updae

---
+++ javascript1/dom/dom.js
@@ -0,0 +1,20 @@
+//alert('hola');
+
+//let cajas=document.getElementsByTagName('div');
+//cajas[0].textContent='Primera Caja';
+
+let cajas=document.getElementsByClassName('caja');
+cajas[2].textContent='Bienvenido';
+
+let caja1 =document.getElementById('cja1');
+//caja1.textContent = 'Primera Caja';
+
+caja1.innerHTML='<h1> Hola </h1>';
+
+// 
+
+
+
+
+
+
